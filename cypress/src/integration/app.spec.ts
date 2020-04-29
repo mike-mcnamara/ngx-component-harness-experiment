22e347899872318c3401cdf0db58a0455f12e158
Michael McNamara <mike.mcnamara@arcadia.io>
2020-04-29 17:32:18 -0400

    switched to cypress e2e framework with empty test

---
+++ cypress/src/integration/app.spec.ts
@@ -0,0 +1,7 @@
+import { getSelect } from '../support/app.po';
+
+describe('ngx-component-harnesses', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display your favorite ocean creature in a sentence', () => {});
+});
---
+++ cypress/src/support/app.po.ts
@@ -0,0 +1,1 @@
+export const getSelect = () => cy.get('app-root');
