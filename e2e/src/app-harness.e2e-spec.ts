4f374066d80dfc2eb5af83872e20dab97c9b2348
Michael McNamara <mike.mcnamara@arcadia.io>
2020-04-29 12:45:08 -0400

    slimmed down tests for protractor based interview

---
+++ e2e/src/app-harness.e2e-spec.ts
@@ -0,0 +1,19 @@
+import { ProtractorHarnessEnvironment } from '@angular/cdk/testing/protractor';
+import { browser, logging } from 'protractor';
+
+import {
+  FavoriteOceanCreatureHarness,
+} from '../../src/app/favorite-ocean-creature/favorite-ocean-creature.harness';
+
+describe('Favorite ocean creature app', () => {
+  beforeEach(async () => {
+    // go to page
+    // load and assign harness
+  });
+
+  it('put your favorite ocean creature in a sentence', async () => {});
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+  });
+});
---
+++ e2e/src/app-po.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { browser, logging } from 'protractor';
+
+import AppPage from './app.po';
+
+describe('Favorite ocean creature app', () => {
+  beforeEach(async () => {
+    // go to page
+    // load and assign harness
+  });
+
+  it('put your favorite ocean creature in a sentence', async () => {});
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+  });
+});
