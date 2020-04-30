2bac4d4287fbe03967b51fdb9790a0b480e0bf41
Michael McNamara <mike.mcnamara@arcadia.io>
2020-04-30 13:47:01 -0400

    test examples using cypress

--- cypress/src/integration/app.spec.ts
+++ cypress/src/integration/app.spec.ts
@@ -1,7 +1,31 @@
 import { getSelect } from '../support/app.po';
 
+import { oceanCreatures } from '../../../src/app/favorite-ocean-creature/ocean-creatures';
+
+const oceanSentence = 'My favorite ocean creature is';
+const displayedOptions = () => cy.get('body .mat-select-panel-wrap .mat-option .mat-option-text');
+
 describe('ngx-component-harnesses', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display your favorite ocean creature in a sentence', () => {});
+  it('displays prompt for user to "Pick your favorite" ocean creature', () => {
+    getSelect().get('app-favorite-ocean-creature') .within(() => {
+      cy.contains('Pick your favorite').should('be.visible');
+      cy.contains(oceanSentence).should('be.visible');
+    });
+  });
+
+  it('has the expected number of ocean creatuures available', () => {
+    getSelect().get('app-favorite-ocean-creature-picker').click();
+    displayedOptions().should('have.length', oceanCreatures.length);
+  });
+
+  oceanCreatures.forEach( (creature) => {
+    it('should display ' + creature['name'] + ' ocean creature in a sentence', () => {
+      getSelect().get('app-favorite-ocean-creature-picker').click();
+      displayedOptions().contains(creature['name']).should('be.visible').click();
+      getSelect().contains('app-favorite-ocean-creature', oceanSentence + ' ' + creature['name']).should('be.visible');
+    });
+  });
+
 });
